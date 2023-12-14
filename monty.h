0136ea7b60fae279b075e8de47cfdadb75237936
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-12-14 21:09:49 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -54,8 +54,8 @@ typedef struct bus_s
 }  bus_t;
 extern bus_t bus;
 
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **head, unsigned int line_number);
+void pall(stack_t **head, unsigned int counter);
 void pint(stack_t **stack, unsigned int line_number),
 void pop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
