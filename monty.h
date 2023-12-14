c23a82b856114577564ca179aca8b49963302a32
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-12-14 21:13:32 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -37,25 +37,9 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
 
-void push(stack_t **head, unsigned int line_number);
-void pall(stack_t **head, unsigned int counter);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number),
 void pop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
