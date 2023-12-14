368dba006854e63fddc2045cb25ecb2a11df8823
Djomoro21 <cherifsylla21@gmail.com>
2023-12-14 22:32:06 +0100

    Monty Project

--- opfun4.c
+++ opfun4.c
@@ -59,16 +59,3 @@ void stack(stack_t **stack, unsigned int line_number)
 
 	var.MODE = 0;
 }
-
-/**
- * queue - s
- * @stack: Double linked list
- * @line_number: File line execution
- */
-void queue(stack_t **stack, unsigned int line_number)
-{
-	(void) line_number;
-	(void) stack;
-
-	var.MODE = 1;
-}
